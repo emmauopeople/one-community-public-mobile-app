b934e6597fb9223f3d3e3ec56907493a90953021
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-02 16:12:59 -0500

    Switch mobile app to real API mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,4 +16,4 @@ export const DEFAULT_CITY = 'Douala';
 
 export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'https://api.cameroonskills.org/api';
 
-export const USE_MOCK_DATA = true;
+export const USE_MOCK_DATA = false;
