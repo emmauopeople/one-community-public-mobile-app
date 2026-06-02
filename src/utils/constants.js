04cfc0132678a8dd3c73901d25e1fc2d8f90ce20
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-02 10:46:52 -0500

    Populate app constants

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const CATEGORIES = [
+  'All Categories',
+  'Carpentry',
+  'Plumbing',
+  'Electrical',
+  'Mechanic',
+  'Tutoring',
+  'Tailoring',
+  'Hairdressing',
+  'Cleaning',
+  'Driving',
+  'Other'
+];
+
+export const DEFAULT_CITY = 'Douala';
+
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'https://api.cameroonskills.org/api';
