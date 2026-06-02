c697fa6f8701956c43096ca04cbcea6ef5e69174
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-02 14:30:12 -0500

    Add data source mode constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,5 @@ export const CATEGORIES = [
 export const DEFAULT_CITY = 'Douala';
 
 export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'https://api.cameroonskills.org/api';
+
+export const USE_MOCK_DATA = true;
