00b70b855c58e74e51b975c845ba711d85e07345
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-02 10:45:35 -0500

    Populate typography theme

---
+++ src/theme/typography.js
@@ -0,0 +1,18 @@
+export const typography = {
+  title: 30,
+  heading: 24,
+  subheading: 18,
+  body: 15,
+  small: 13,
+  tiny: 11,
+  lineHeight: {
+    title: 38,
+    heading: 30,
+    body: 22
+  },
+  weights: {
+    regular: '400',
+    medium: '600',
+    bold: '700'
+  }
+};
